97c8a744c984c2a4406f5f9dc713f4408ff1a8a5
P4yDay <46981551+P4yDay@users.noreply.github.com>
2020-05-01 08:43:31 -0400

    did a thing!

--- poketimes/src/Components/Home.js
+++ poketimes/src/Components/Home.js
@@ -1,50 +1,45 @@
-import React, { Component } from 'react'
-import axios from 'axios';
-import{ Link }from 'react-router-dom';
+  
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Pokeball from '../pokeb.jpg';
-class Home extends Component{
-    state = {
-        posts: [ ]
-    }
+import { connect } from 'react-redux';
 
-    componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(res => {
-            console.log(res)
-            this.setState({
-                posts: res.data.slice(0,10)
-            })
-        })
-    }
-
-    render(){
-        const { posts } = this.state
-        const postList = posts.length ? (
-            posts.map(post => {
-                return (
-                    <div className="post card" key={post.id}>
-                        <img src={Pokeball} alt="a pokeball" />
-                        <div className="card-content">
-                            <Link to={'/' + post.id}>
-                            <span className="card-title"> {post.title} </span>
-                            </Link>
-                            <p>{post.body}</p>
-                        </div>
-                    </div>
-                )
-            })
-        ) : (
-            <div className="center"> No Post yet </div>
-        )
-
-
-        return(
-            <div className="container">
-                <h4 className="center">Home</h4>
-                <p>{postList}</p>
+class Home extends Component {
+  render(){
+    const { posts } = this.props
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <img src={Pokeball} alt="A Pokeball" />
+            <div className="card-content">
+              <Link to={'/' + post.id}>
+                <span className="card-title red-text">{post.title}</span>
+              </Link>
+              <p>{post.body}</p>
             </div>
+          </div>
         )
-    }
+      })
+    ) : (
+      <div className="center">No posts to show</div>
+    );
+
+    return (
+      <div>
+        <div className="container home">
+          <h4 className="center">Home</h4>
+          {postList}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default Home
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts
+  }
+}
+
+export default connect(mapStateToProps)(Home)
--- poketimes/src/Components/Post.js
+++ poketimes/src/Components/Post.js
@@ -1,39 +1,36 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+import { connect } from 'react-redux'
 
 class Post extends Component {
-    state = {
-        post: null
-    }
+  render() {
+    console.log(this.props)
+    const post = this.props.post ? (
+      <div className="post">
+        <h4 className="center">{this.props.post.title}</h4>
+        <p>{this.props.post.body}</p>
+      </div>
+    ) : (
+      <div className="center">Loading post...</div>
+    );
 
-    componentDidMount(){
-        let id = this.props.match.params.post_id;
-        axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
-        .then(res => {
-            this.setState({
-                post: res.data
-            })
-        })
-        
-    }
-    
-    render() {
-        
-        const post = this.state.post ? (
-            <div className="post">
-                <h4 className="center">{this.state.post.title} </h4>
-                 <p> {this.state.post.body}</p>
-            </div>
-        ) : (
-            <div className="center"> Loading..... </div>
-        )
-
-        return (
-            <div className="container">
-                {post}
-            </div>
-        )
-    }
+    return (
+      <div className="container">
+        {post}
+      </div>
+    )
+  }
 }
 
-export default Post;
+const mapStateToProps = (state, ownProps) => {
+  let id = ownProps.match.params.post_id;
+  return {
+    post: state.posts.find(post => post.id === id) 
+  }
+}
+
+const mapDispacthToProps = (dispatch) => {
+  return {
+    deletePost: (id) => { dispatch({type: 'DELETE_POST', id: id}) }
+  }
+}
+export default connect(mapStateToProps, mapDispacthToProps)(Post)
